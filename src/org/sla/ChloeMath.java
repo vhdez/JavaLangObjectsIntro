7ccc3db586a6ba8587c851a79ca6b852d87d8131
csimmons20 <csimmons20@scienceleadership.org>
2018-10-03 08:54:45 -0400

    12x - 12 = 72

--- src/org/sla/ChloeMath.java
+++ src/org/sla/ChloeMath.java
@@ -3,18 +3,18 @@ package org.sla;
 public class ChloeMath {
     public static void main(String[] args) {
         // 12x - 12 = 72
-        long a = 12;
-        long b = 72;
-        long c = 12;
+        long x = 12;
+        long y = 72;
+        long z = 12;
 
 
         //Solve for x
-        System.out.println(a + "x - " + c + " = " + b);
+        System.out.println(x + "x - " + z + " = " + y);
 
-        long newValue = Math.addExact(b, c);
-        System.out.println(a + "x = " + newValue);
+        long newValue = Math.addExact(y, z);
+        System.out.println(x + "x = " + newValue);
 
-        long x = newValue/a;
-        System.out.println("x = " + x);
+        long e = newValue/x;
+        System.out.println("x = " + e);
     }
 }
